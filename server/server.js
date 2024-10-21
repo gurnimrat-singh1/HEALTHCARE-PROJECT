9d35d4cb581b4c404ebe87199c062134afa09d8d
Gurnimrat <singhmarwaha2004@gmail.com>
2024-10-21 15:21:17 +0530

    good

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDb= require('./config/dbConnection');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+connectDb();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(errorHandler);
+
+app.get('/', (req, res) => {
+    res.send('working');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http:localhost:${port}`);
+}) ;
