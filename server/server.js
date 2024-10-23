a9c29ab7d1b3149d46b244c26cc9a7050558c808
Gurnimrat <singhmarwaha2004@gmail.com>
2024-10-23 15:48:36 +0530

    hbs

--- server/server.js
+++ server/server.js
@@ -1,25 +1,62 @@
-const express = require('express');
-const connectDb= require('./config/dbConnection');
-const errorHandler = require('./middleware/errorHandler');
-const cors = require('cors');
-const port = process.env.PORT || 5000;
+//Framework Configuration
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
+const hbs = require("hbs");
+const path = require("path");
 
-//env file config
-const dotenv=require("dotenv");
+const dotenv = require("dotenv");
 dotenv.config();
+
+// var hbs=require('hbs');
+//hbs.registerePartials(__dirname+'/views/partials',function(err){});
+hbs.registerPartials(__dirname + '/views/partials', function(err) {});
 
 connectDb();
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
 app.use(errorHandler);
 
-app.get('/', (req, res) => {
-    res.send('working');
+// ERROR handling middleware
+app.use(errorHandler);
+
+app.set('view engine', 'hbs');
+
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("working");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http:localhost:${port}`);
-}) ;
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
+
+
+app.get("/allusers",(req,res)=>{
+    res.render("users",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
+})
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+// APP CONFIG START
+app.listen(port, () =>{
+    console.log(`Server running in port http://localhost:${port}`);
+});
